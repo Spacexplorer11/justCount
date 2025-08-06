a24fd0055f50c9edc80e4a28699976e1d5588397
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-06 22:27:32 +0100

    Added header, main and footer tags as boilerplate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <div class="flex flex-col text-center">
+	<header>
 <h1 class="text-6xl mb-5 mt-5">Just Count</h1>
 <h6 class="text-2xl">A simple counter website so you can count how many times you do your tedious tasks that need to be done multiple times a day</h6>
+	</header>
+	<main>
+
+	</main>
+	<footer>
+
+	</footer>
 </div>
